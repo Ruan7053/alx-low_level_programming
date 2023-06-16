3d944d5b63a25cfb5dd3115b2ed4a58c1f2d4b16
Ruan Olivier <ruan7053@gmail.com>
2023-06-16 06:54:28 -0700

    Betty style spacing changes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,10 +22,12 @@ int main(void)
 	if (lastnum > 5)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, lastnum);
-	} else if (lastnum == 0)
+	}
+	else if (lastnum == 0)
 	{
 	printf("Last digit of %d is %d and is 0\n", n, lastnum);
-	} else
+	}
+	else
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnum);
 	}
