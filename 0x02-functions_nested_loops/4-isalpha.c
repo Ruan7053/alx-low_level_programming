7f2b919cd9808fd1d9178ed05a0121f9ba9f959d
Ruan Olivier <ruan7053@gmail.com>
2023-06-20 04:18:49 -0700

    Corrected a speling mistake in 4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,13 @@
 #include "main.h"
-
 /**
- * _isaplha - checks the input character if it is alphabetic
+ * _isalpha - checks the input character if it is alphabetic
  *
  * @c: character that is tested
  *
  * Return: 1 if true, 0 if false
  *
  */
-int _isplha(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'))
 	{
