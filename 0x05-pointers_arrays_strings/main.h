a80e427dad82af682ad02d23014c294b08303425
Ruan Olivier <ruan7053@gmail.com>
2023-06-26 06:43:45 -0700

    Corrected a typo in main.h putcha > putchar

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
-#ifndef FILE_MAIN
-#define FAIL_MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putcha(char c);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
